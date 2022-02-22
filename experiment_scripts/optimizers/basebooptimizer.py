fixing normalization issue

experiment_scripts/optimizers/basebooptimizer.py
@@ -3,11 +3,11 @@ import sys
 from typing import Optional
 
 import torch
+from torch import Tensor
+
 from botorch.generation import gen_candidates_torch
 from botorch.optim import gen_batch_initial_conditions
 from botorch.optim import optimize_acqf
-from torch import Tensor
-
 from .baseoptimizer import BaseOptimizer
 from .utils import timeit
 
@@ -64,7 +64,9 @@ class BaseBOOptimizer(BaseOptimizer):
     def _sgd_optimize_aqc_fun(self, acq_fun: callable, **kwargs) -> Tensor:
         """Use multi-start Adam SGD over multiple seeds"""
 
-        bounds_normalized = torch.hstack([torch.zeros((self.dim, 1)), torch.ones(( self.dim, 1))])
+        bounds_normalized = torch.vstack(
+            [torch.zeros((1, self.dim)), torch.ones((1, self.dim))]
+        )
         # This optimizer uses "L-BFGS-B" by default. If specified, optimizer is Adam.
         if self.optional["OPTIMIZER"] == "Adam":
             initial_conditions = gen_batch_initial_conditions(

experiment_scripts/optimizers/optimizer.py
@@ -3,6 +3,10 @@ import pickle as pkl
 from typing import Optional
 
 import torch
+from gpytorch.kernels import RBFKernel, ScaleKernel
+from gpytorch.mlls import ExactMarginalLogLikelihood
+from torch import Tensor
+
 from botorch.acquisition import PosteriorMean
 from botorch.fit import fit_gpytorch_model
 from botorch.models import FixedNoiseGP, SingleTaskGP
@@ -10,10 +14,6 @@ from botorch.optim import optimize_acqf
 from botorch.optim.initializers import gen_batch_initial_conditions
 from botorch.utils import standardize
 from botorch.utils.transforms import unnormalize, normalize
-from gpytorch.kernels import RBFKernel, ScaleKernel
-from gpytorch.mlls import ExactMarginalLogLikelihood
-from torch import Tensor
-
 from .basebooptimizer import BaseBOOptimizer
 from .utils import timeit
 
@@ -48,7 +48,6 @@ class Optimizer(BaseBOOptimizer):
         self.base_seed = base_seed
         self.nz = nz
         self.save_folder = save_folder
-        self.bounds = testfun.bounds
         if kernel_str == "RBF":
             self.covar_module = ScaleKernel(
                 RBFKernel(ard_num_dims=self.dim),
@@ -67,12 +66,11 @@ class Optimizer(BaseBOOptimizer):
         return y
 
     def _update_model(self, X_train: Tensor, Y_train: Tensor):
-        X_train_normalized = normalize(X=X_train, bounds=self.bounds)
         Y_train_standarized = standardize(Y_train)
 
         if self.optional["NOISE_OBJECTIVE"]:
             self.model = SingleTaskGP(
-                train_X=X_train_normalized,
+                train_X=X_train,
                 train_Y=Y_train_standarized,
                 covar_module=self.covar_module,
             )
@@ -80,7 +78,7 @@ class Optimizer(BaseBOOptimizer):
             NOISE_VAR = torch.Tensor([1e-4])
 
             self.model = FixedNoiseGP(
-                train_X=X_train_normalized,
+                train_X=X_train,
                 train_Y=Y_train_standarized,
                 covar_module=self.covar_module,
                 train_Yvar=NOISE_VAR.expand_as(Y_train_standarized),
@@ -101,7 +99,9 @@ class Optimizer(BaseBOOptimizer):
         assert self.y_train is not None
         "Include data to find best posterior mean"
 
-        bounds_normalized = torch.hstack([torch.zeros((self.dim, 1)), torch.ones(( self.dim, 1))])
+        bounds_normalized = torch.vstack(
+            [torch.zeros((1, self.dim)), torch.ones((1, self.dim))]
+        )
 
         # generate initialisation points
         batch_initial_conditions = gen_batch_initial_conditions(
@@ -133,6 +133,7 @@ class Optimizer(BaseBOOptimizer):
             num_restarts=self.optional["NUM_RESTARTS"],
             raw_samples=self.optional["RAW_SAMPLES"],
         )
+
         return argmax_pmean
 
     def get_next_point(self):
@@ -158,7 +159,7 @@ class Optimizer(BaseBOOptimizer):
             "problem": self.f.problem,
             "method_times": self.method_time,
             "OC": self.performance,
-            "x": self.x_train,
+            "x": unnormalize(self.x_train, self.bounds),
             "y": self.y_train,
             "kernel": self.kernel_name,
             "gp_lik_noise": self.gp_likelihood_noise,

experiment_scripts/optimizers/utils.py
@@ -38,8 +38,8 @@ def lhc(
 
     if dim is not None:
         assert (lb is None) and (ub is None), "give dim OR bounds"
-        lb = torch.zeros(dim)
-        ub = torch.ones(dim)
+        lb = torch.zeros(dim, dtype=torch.double)
+        ub = torch.ones(dim, dtype=torch.double)
 
     else:
         assert (lb is not None) and (ub is not None), "give dim OR bounds"
@@ -48,7 +48,7 @@ def lhc(
         dim = len(lb)
         assert len(lb) == len(ub), f"bounds are not same shape:{len(lb)}!={len(ub)}"
 
-    x = torch.zeros((n, dim))
+    x = torch.zeros((n, dim), dtype=torch.double)
     if n > 0:
         for d in range(dim):
             x[:, d] = (torch.randperm(n) + torch.rand(n)) * (1 / n)
@@ -68,8 +68,7 @@ def timeit(method):
                 kw["log_time"][name].append((te - ts))
             else:
                 kw["log_time"][name] = [te - ts]
-        else:
-            print("%r  %2.2f ms" % (method.__name__, (te - ts)))
+
         return result
 
     return timed
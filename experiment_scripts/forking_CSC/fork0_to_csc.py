8c823dd5f82dc04edbe9b57952da3b34768e79b0
Juan Ungredda <j.ungredda@warwick.ac.uk>
2022-02-27 20:58:56 +0000

    setting csc machines

--- experiment_scripts/forking_CSC/fork0_to_csc.py
+++ experiment_scripts/forking_CSC/fork0_to_csc.py
@@ -133,7 +133,7 @@ ALL_CSC_NAMES = [
 # U = [0,2,3,4,5,6,7,8]
 # python fork0_to_csc.py /home/rawsys/matjiu/BOPL_Cornell-Warwick/experiments/control.py 14 --first_fork kumeta --basedir /home/rawsys/matjiu/RESULTS_BOPL --conda BOPL
 
-U = range(len(ALL_CSC_NAMES))  # [3, 5, 6]
+U = [3, 4, 5, 8, 9, 10, 11]
 
 CSC_NAMES = [ALL_CSC_NAMES[i] for i in U]
 

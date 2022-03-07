300d3160b066fbb724d5e448aafc65cd55e4cba8
Juan Ungredda <j.ungredda@warwick.ac.uk>
2022-03-07 14:29:12 +0000

    GP synthetic function MCKG 3

--- experiment_scripts/csc_control.py
+++ experiment_scripts/csc_control.py
@@ -48,22 +48,22 @@ def run(args):
     number_of_csc_machines = len(U)
     # print(number_of_csc_machines)
     seed = 0
-    for _ in range(30):
+    for _ in range(33):
         experiment_names = [
-            # "MCKG_GP_synthetic_3_dim2_l0.1",
-            #                 "MCKG_GP_synthetic_3_dim2_l0.4",
+                            "MCKG_GP_synthetic_3_dim2_l0.1",
+                            "MCKG_GP_synthetic_3_dim2_l0.4",
             #                 "MCKG_GP_synthetic_3_dim2_l1",
-            #                 "MCKG_GP_synthetic_3_dim4_l0.1",
-            #                 "MCKG_GP_synthetic_3_dim4_l0.4",
+                            "MCKG_GP_synthetic_3_dim4_l0.1",
+                            "MCKG_GP_synthetic_3_dim4_l0.4",
             #                 "MCKG_GP_synthetic_3_dim4_l1",
             #                 "MCKG_GP_synthetic_3_dim6_l0.1",
             #                 "MCKG_GP_synthetic_3_dim6_l0.4",
             #                 "MCKG_GP_synthetic_3_dim6_l1",
-                            "DISCKG_GP_synthetic_1000_dim2_l0.1",
-                            "DISCKG_GP_synthetic_1000_dim2_l0.4",
+            #                 "DISCKG_GP_synthetic_1000_dim2_l0.1",
+            #                 "DISCKG_GP_synthetic_1000_dim2_l0.4",
                             # "DISCKG_GP_synthetic_1000_dim2_l1",
-                            "DISCKG_GP_synthetic_1000_dim4_l0.1",
-                            "DISCKG_GP_synthetic_1000_dim4_l0.4",
+                            # "DISCKG_GP_synthetic_1000_dim4_l0.1",
+                            # "DISCKG_GP_synthetic_1000_dim4_l0.4",
                             # "DISCKG_GP_synthetic_1000_dim4_l1",
                             # "DISCKG_GP_synthetic_1000_dim6_l0.1",
                             # "DISCKG_GP_synthetic_1000_dim6_l0.4",

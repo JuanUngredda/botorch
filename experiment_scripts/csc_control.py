d9becb2bd1a1b03bab735477d42cd5692199f1a2
Juan Ungredda <j.ungredda@warwick.ac.uk>
2022-05-28 23:35:36 +0100

    noisy experiment platform

--- experiment_scripts/csc_control.py
+++ experiment_scripts/csc_control.py
@@ -55,7 +55,10 @@ def run(args):
                             # "noisy_BNH_macKG_experiments",
                             # "noisy_BNH_benchmarks_experiments"
                             # "SRN_macKG_experiments",
-                            "SRN_benchmarks_experiments"
+                            "ConstrainedBraninCurrin_benchmarks_experiments",
+            "CONSTR_benchmarks_experiments",
+            "C2DTLZ2_benchmarks_experiments",
+            "OSY_benchmarks_experiments"
                             ]
 
         # experiment_names = ["OSY_f50_experiments"]

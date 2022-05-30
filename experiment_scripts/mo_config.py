f0ea920107fe28e9bd9a15cced45a969dc900376
Juan Ungredda <j.ungredda@warwick.ac.uk>
2022-05-30 11:24:36 +0100

    noisy experiment platform

--- experiment_scripts/mo_config.py
+++ experiment_scripts/mo_config.py
@@ -134,7 +134,7 @@ CONFIG_DICT = {
     "noisy_SRN_macKG_experiments": {
         "problems": ["SRN"],
         "method": ["macKG", "pen-maKG"],
-        "noise_lvl": None,
+        "noise_lvl": 1,
         "output_dim": 2,
         "input_dim": 2,
         "number_of_scalarizations": 10,
@@ -153,7 +153,7 @@ CONFIG_DICT = {
     "noisy_SRN_benchmarks_experiments": {
         "problems": ["SRN"],
         "method": ["cEHI", "cParEGO"],
-        "noise_lvl": None,
+        "noise_lvl": 1,
         "output_dim": 2,
         "input_dim": 2,
         "number_of_scalarizations": 10,
@@ -169,6 +169,7 @@ CONFIG_DICT = {
         "num_restarts_acq_optimizer": 1,
         "raw_samples_acq_optimizer": 100,
     },
+
     "ConstrainedBraninCurrin_macKG_experiments": {
         "problems": ["ConstrainedBraninCurrin"],
         "method": ["macKG", "pen-maKG"],
@@ -192,6 +193,45 @@ CONFIG_DICT = {
         "problems": ["ConstrainedBraninCurrin"],
         "method": ["cEHI", "cParEGO"],
         "noise_lvl": None,
+        "output_dim": 2,
+        "input_dim": 2,
+        "number_of_scalarizations": 10,
+        "num_samples_initial_design": 6,
+        "num_max_evaluatations": 100,
+        "utility_model": ["Tche"],
+        "num_discrete_points": 50,
+        "num_fantasies": 128,
+        "num_restarts_inner_optimizer": 1,
+        "raw_samples_inner_optimizer": 100,
+        "acquisition_optimizer":
+            "L-BFGS-B",  # "L-BFGS-B" or "Adam"
+        "num_restarts_acq_optimizer": 1,
+        "raw_samples_acq_optimizer": 100,
+    },
+
+    "noisy_ConstrainedBraninCurrin_macKG_experiments": {
+        "problems": ["ConstrainedBraninCurrin"],
+        "method": ["macKG", "pen-maKG"],
+        "noise_lvl": 0.2,
+        "output_dim": 2,
+        "input_dim": 2,
+        "number_of_scalarizations": 10,
+        "num_samples_initial_design": 6,
+        "num_max_evaluatations": 100,
+        "utility_model": ["Tche"],
+        "num_discrete_points": 50,
+        "num_fantasies": 3,
+        "num_restarts_inner_optimizer": 1,
+        "raw_samples_inner_optimizer": 100,
+        "acquisition_optimizer":
+            "L-BFGS-B",  # "L-BFGS-B" or "Adam"
+        "num_restarts_acq_optimizer": 1,
+        "raw_samples_acq_optimizer": 100,
+    },
+    "noisy_ConstrainedBraninCurrin_benchmarks_experiments": {
+        "problems": ["ConstrainedBraninCurrin"],
+        "method": ["cEHI", "cParEGO"],
+        "noise_lvl": 0.2,
         "output_dim": 2,
         "input_dim": 2,
         "number_of_scalarizations": 10,
@@ -247,6 +287,46 @@ CONFIG_DICT = {
         "raw_samples_acq_optimizer": 100,
     },
 
+    "noisy_CONSTR_macKG_experiments": {
+        "problems": ["CONSTR"],
+        "method": ["macKG", "pen-maKG"],
+        "noise_lvl": 0.2,
+        "output_dim": 2,
+        "input_dim": 2,
+        "number_of_scalarizations": 10,
+        "num_samples_initial_design": 6,
+        "num_max_evaluatations": 100,
+        "utility_model": ["Tche"],
+        "num_discrete_points": 50,
+        "num_fantasies": 3,
+        "num_restarts_inner_optimizer": 1,
+        "raw_samples_inner_optimizer": 100,
+        "acquisition_optimizer":
+            "L-BFGS-B",  # "L-BFGS-B" or "Adam"
+        "num_restarts_acq_optimizer": 1,
+        "raw_samples_acq_optimizer": 100,
+    },
+    "noisy_CONSTR_benchmarks_experiments": {
+        "problems": ["CONSTR"],
+        "method": ["cEHI", "cParEGO"],
+        "noise_lvl": 0.2,
+        "output_dim": 2,
+        "input_dim": 2,
+        "number_of_scalarizations": 10,
+        "num_samples_initial_design": 6,
+        "num_max_evaluatations": 100,
+        "utility_model": ["Tche"],
+        "num_discrete_points": 50,
+        "num_fantasies": 128,
+        "num_restarts_inner_optimizer": 1,
+        "raw_samples_inner_optimizer": 100,
+        "acquisition_optimizer":
+            "L-BFGS-B",  # "L-BFGS-B" or "Adam"
+        "num_restarts_acq_optimizer": 1,
+        "raw_samples_acq_optimizer": 100,
+    },
+
+
     "C2DTLZ2_macKG_experiments": {
         "problems": ["C2DTLZ2"],
         "method": ["macKG", "pen-maKG"],
@@ -286,6 +366,45 @@ CONFIG_DICT = {
         "raw_samples_acq_optimizer": 100,
     },
 
+    "noisy_C2DTLZ2_macKG_experiments": {
+        "problems": ["C2DTLZ2"],
+        "method": ["macKG", "pen-maKG"],
+        "noise_lvl": 0.01,
+        "output_dim": 2,
+        "input_dim": 3,
+        "number_of_scalarizations": 10,
+        "num_samples_initial_design": 8,
+        "num_max_evaluatations": 100,
+        "utility_model": ["Tche"],
+        "num_discrete_points": 50,
+        "num_fantasies": 3,
+        "num_restarts_inner_optimizer": 1,
+        "raw_samples_inner_optimizer": 100,
+        "acquisition_optimizer":
+            "L-BFGS-B",  # "L-BFGS-B" or "Adam"
+        "num_restarts_acq_optimizer": 1,
+        "raw_samples_acq_optimizer": 100,
+    },
+    "noisy_C2DTLZ2_benchmarks_experiments": {
+        "problems": ["C2DTLZ2"],
+        "method": ["cEHI", "cParEGO"],
+        "noise_lvl": 0.01,
+        "output_dim": 2,
+        "input_dim": 3,
+        "number_of_scalarizations": 10,
+        "num_samples_initial_design": 8,
+        "num_max_evaluatations": 100,
+        "utility_model": ["Tche"],
+        "num_discrete_points": 50,
+        "num_fantasies": 128,
+        "num_restarts_inner_optimizer": 1,
+        "raw_samples_inner_optimizer": 100,
+        "acquisition_optimizer":
+            "L-BFGS-B",  # "L-BFGS-B" or "Adam"
+        "num_restarts_acq_optimizer": 1,
+        "raw_samples_acq_optimizer": 100,
+    },
+
     "OSY_macKG_experiments": {
         "problems": ["OSY"],
         "method": ["macKG", "pen-maKG"],
@@ -309,6 +428,45 @@ CONFIG_DICT = {
         "problems": ["OSY"],
         "method": ["cEHI", "cParEGO"],
         "noise_lvl": None,
+        "output_dim": 2,
+        "input_dim": 6,
+        "number_of_scalarizations": 10,
+        "num_samples_initial_design": 14,
+        "num_max_evaluatations": 100,
+        "utility_model": ["Tche"],
+        "num_discrete_points": 50,
+        "num_fantasies": 128,
+        "num_restarts_inner_optimizer": 1,
+        "raw_samples_inner_optimizer": 100,
+        "acquisition_optimizer":
+            "L-BFGS-B",  # "L-BFGS-B" or "Adam"
+        "num_restarts_acq_optimizer": 1,
+        "raw_samples_acq_optimizer": 100,
+    },
+
+    "noisy_OSY_macKG_experiments": {
+        "problems": ["OSY"],
+        "method": ["macKG", "pen-maKG"],
+        "noise_lvl": 1.0,
+        "output_dim": 2,
+        "input_dim": 6,
+        "number_of_scalarizations": 10,
+        "num_samples_initial_design": 14,
+        "num_max_evaluatations": 100,
+        "utility_model": ["Tche"],
+        "num_discrete_points": 50,
+        "num_fantasies": 3,
+        "num_restarts_inner_optimizer": 1,
+        "raw_samples_inner_optimizer": 100,
+        "acquisition_optimizer":
+            "L-BFGS-B",  # "L-BFGS-B" or "Adam"
+        "num_restarts_acq_optimizer": 1,
+        "raw_samples_acq_optimizer": 100,
+    },
+    "noisy_OSY_benchmarks_experiments": {
+        "problems": ["OSY"],
+        "method": ["cEHI", "cParEGO"],
+        "noise_lvl": 1.0,
         "output_dim": 2,
         "input_dim": 6,
         "number_of_scalarizations": 10,

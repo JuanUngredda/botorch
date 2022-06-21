df16a8426c946d3e5d5b7c313686dfc45f07e486
Juan Ungredda <j.ungredda@warwick.ac.uk>
2022-06-21 08:13:19 +0100

    HYBRIDKG

--- experiment_scripts/config.py
+++ experiment_scripts/config.py
@@ -1029,6 +1029,39 @@ CONFIG_DICT = {
         ],  # "L-BFGS-B" or "Adam"
         "num_restarts_acq_optimizer": [10],
         "raw_samples_acq_optimizer": [100]},
+
+    "HYBRIDKG_GP_synthetic_3_dim6_l0.1": {
+        "problems": ["GP_synthetic"],
+        "num_input_dim": [6],
+        "lengthscale": [0.1],
+        "method": ["HYBRIDKG"],
+        "num_samples_initial_design": [14],
+        "num_max_evaluatations": [100],
+        "num_discrete_points": [1000],
+        "num_fantasies": [10],
+        "num_restarts_inner_optimizer": [1],
+        "raw_samples_inner_optimizer": [100],
+        "acquisition_optimizer": [
+            "L-BFGS-B"
+        ],  # "L-BFGS-B" or "Adam"
+        "num_restarts_acq_optimizer": [10],
+        "raw_samples_acq_optimizer": [100]},
+    "HYBRIDKG_GP_synthetic_10_dim6_l0.1": {
+        "problems": ["GP_synthetic"],
+        "num_input_dim": [6],
+        "lengthscale": [0.1],
+        "method": ["HYBRIDKG"],
+        "num_samples_initial_design": [14],
+        "num_max_evaluatations": [100],
+        "num_discrete_points": [1000],
+        "num_fantasies": [10],
+        "num_restarts_inner_optimizer": [1],
+        "raw_samples_inner_optimizer": [100],
+        "acquisition_optimizer": [
+            "L-BFGS-B"
+        ],  # "L-BFGS-B" or "Adam"
+        "num_restarts_acq_optimizer": [10],
+        "raw_samples_acq_optimizer": [100]},
 }
 
 
